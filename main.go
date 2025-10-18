67ed183b36fe68534f113bdd2d16281ba74ec88b
Lucas <46531925+cz-lucas@users.noreply.github.com>
2025-10-18 20:39:09 +0000

    Added loop to run request ever x seconds

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/joho/godotenv"
 )
@@ -11,8 +13,22 @@ func main() {
 
 	godotenv.Load(".env")
 	var ttnRequestUrl = os.Getenv("TTN_BASE_URL") + os.Getenv("TTN_GATEWAY_ID") + os.Getenv("TTN_URL_STATS_SUFFIX")
-	log.Println(ttnRequestUrl)
-
 	var apiService = NewTTNApiService(ttnRequestUrl, os.Getenv("TTN_API_KEY"))
-	log.Println(apiService.Get())
+
+	intervalInSeconds, err := strconv.Atoi(os.Getenv("READ_INTERVAL"))
+
+	if err != nil {
+		log.Fatal("Invalid READ_INTERVAL:", err)
+	}
+
+	ticker := time.NewTicker(time.Duration(intervalInSeconds) * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ticker.C:
+			log.Println("Doing work at", time.Now())
+			log.Println(apiService.Get())
+		}
+	}
 }
